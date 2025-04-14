7dc5f27feff1c08f6a1693b040a2b3fa96e6f3a3
Arhan1606 <arhanjain1606@gmail.com>
2025-04-14 12:11:10 +0530

    Code Updated

--- Assingment1/main.cpp
+++ Assingment1/main.cpp
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sra_board.h"
 
+void app_main()
+{
+    ESP_ERROR_CHECK(enable_bar_graph());
+    // enable_bar_graph() turns on the gpio pins, if it succeeds it returns ESP_OK else it returns ESP_FAIL
+    // If the argument of ESP_ERROR_CHECK() is not equal ESP_OK, then an error message is printed on the console, and abort() is called. 
+        
+    
+    while(1) //Starting the while loop so that it can run infinite times
+        { 
+
+            ESP_ERROR_CHECK(set_bar_graph(0xAA)); //0xAA is the hexadecimal for even position led's
+            // So using the function even led's are on
+            vTaskDelay(500 / portTICK_PERIOD_MS); // delay of 500 ms
+
+            ESP_ERROR_CHECK(set_bar_graph(0x55)); //0x55 is the hexadecimal for odd position led's
+            // So using the function odd led's are on
+            vTaskDelay(500/portTICK_PERIOD_MS); // delay of 500 ms
+
+
+        }
+}
